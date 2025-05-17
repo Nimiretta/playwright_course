181ca6f744c383e08ceedd84feca25e742fca1af
Tatiana Korol <tatjana.deriugina@gmail.com>
2025-05-17 23:45:07 +0200

    feat: copy code from lecture

---
+++ src/ui/pages/home.page.ts
@@ -0,0 +1,22 @@
+import { Locator } from '@playwright/test';
+import { ModuleName } from 'types';
+import { SalesPortalPage } from './salesPortal.page';
+
+export class HomePage extends SalesPortalPage {
+  readonly title = this.page.locator('.welcome-text');
+  readonly customersButton = this.page.getByRole('link', { name: 'Customer' });
+  readonly productsButton = this.page.getByRole('link', { name: 'Products' });
+  readonly ordersButton = this.page.getByRole('link', { name: 'Orders' });
+
+  readonly uniqueElement = this.title;
+
+  async clickModuleButton(moduleName: ModuleName) {
+    const moduleButtons: Record<ModuleName, Locator> = {
+      Customers: this.customersButton,
+      Products: this.productsButton,
+      Orders: this.ordersButton,
+    };
+
+    await moduleButtons[moduleName].click();
+  }
+}
